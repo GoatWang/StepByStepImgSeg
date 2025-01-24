60d97d9e09000a3ab7ae33524cd3fa5239a360a0
GoatWang <jeremy4555@yahoo.com.tw>
2025-01-24 07:32:31 +0000

    add setup script

--- setup_paperspace.sh
+++ setup_paperspace.sh
@@ -2,3 +2,14 @@ apt install -y tmux
 pip install transformers -U
 pip install pycocotools
 pip install 'accelerate>=0.26.0'
+
+# install flash attention 2: ImportError: FlashAttention2 has been toggled on, but it cannot be used due to the following error: the package flash_attn seems to be not installed. Please refer to the documentation of https://huggingface.co/docs/transformers/perf_infer_gpu_one#flashattention-2 to install Flash Attention 2.
+# pip install flash-attn --no-build-isolation
+
+mkdir -p temp
+if [ ! -e "/notebooks/StepByStepImgSeg/temp/LLaVA" ]; then
+    cd /notebooks/StepByStepImgSeg/temp
+    git clone https://github.com/haotian-liu/LLaVA
+fi
+export PYTHONPATH=/notebooks/StepByStepImgSeg/temp/llava:$PYTHONPATH
+
